07dfc2cca094bee9715b1f8cbcd19c76a4d7a612
kaataknut <kaataknut@gmail.com>
2016-10-04 16:20:54 +0200

    Added simple evaluator

--- oppgave-1/tokenize.hs
+++ oppgave-1/tokenize.hs
@@ -18,6 +18,9 @@ tokenize (x:xs) =
 data Ast = Number Integer | Name String | App Ast [Ast] | Block [Ast] | Case Ast [Ast] | Bool Ast Ast Ast | Default | Set String Ast | Lambda String Ast
   deriving (Eq, Show, Ord)
 
+type Context = [(String, Integer)]
+type Memory  = [(Integer, Integer -> Maybe Ast)]
+
 -- Primitive helper functions for improved readibility
 parseNumber::String -> Integer
 parseNumber x = read x
@@ -25,27 +28,26 @@ parseNumber x = read x
 isFunc::String -> Bool
 isFunc x = x == "+" || x == "-" || x == "/" || x == "*"
 
+evalNumber::Ast -> Integer
+evalNumber (Number a) = a
+
 -- Parse functions
--- Returns the Ast without the [String] from parseBlock
--- ["(", "1", ",", "2", ")", "+", ";"]
 parse::String -> Ast
 parse s = fst $ parseBlock(tokenize s)
 
 parseBlock::[String] -> (Ast, [String])
 parseBlock xs
   | last xs == ";" = (Block [expr], [])
-  | otherwise        = error "Unknown parsing error!"
+  | otherwise      = error "Unknown parsing error!"
   where
     (expr, rest) = parseExpr xs
 
--- [",", "2", ")", "+", ";"]
 parseExpr::[String] -> (Ast, [String])
 parseExpr (x:xs) 
   | isDigit (head x) = (Number (parseNumber x), xs)
   | x == "("         = parseApp xs
   | otherwise        = error "Unknown expression error!"
 
--- ["(", "1", ",", "2", ")", "+"]
 parseApp::[String] -> (Ast, [String])
 parseApp xs
   | isOp && head rest1 == ")"  = ((App (Name op) [a, b]), rest)
@@ -56,3 +58,43 @@ parseApp xs
     op         = rest1 !! 1
     isOp       = isFunc op
     rest       = drop 2 rest1
+
+-- Block [App (Name "+") [App (Name "-") [Number 10,Number 2],Number 4]]
+eval::Ast -> Context -> Memory -> (Ast, Context, Memory)
+eval (Number a) context memory = (Number a, context, memory)
+eval (Block [expr]) context memory = eval expr context memory
+
+simpleEval::Ast -> Ast
+simpleEval (Number a)             = Number a
+simpleEval (Block [expr])         = simpleEval expr
+simpleEval (App (Name op) [a,b])
+  | op == "+" = Number(evalNumber e1 + evalNumber e2)
+  | op == "-" = Number(evalNumber e1 - evalNumber e2)
+  | op == "*" = Number(evalNumber e1 * evalNumber e2)
+  | op == "/" = Number(evalNumber e1 `div` evalNumber e2)
+  | otherwise = error "Unsupporteed operator!"
+  where
+    e1 = simpleEval a
+    e2 = simpleEval b
+
+run::String -> Ast
+run s = simpleEval (parse s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
