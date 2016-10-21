25ca8f8f69dad1975066b5434b8585d3afc2cdfa
kaataknut <kaataknut@gmail.com>
2016-10-21 02:45:25 +0200

    Tokenizer and parser cleaned up.

--- oppgave-1/revised.hs
+++ oppgave-1/revised.hs
@@ -8,7 +8,11 @@ data Ast = Number Integer | Name String | App Ast [Ast] | Block [Ast] | Case Ast
   deriving (Eq, Show, Ord)
 
 
-delimiters = "+-*/,;()"
+-- Helper functions
+isReservedWord :: String -> Bool
+isReservedWord str = elem str ["set", "lambda", "case", "otherwise"]
+
+delimiters = "+-*/,;()."
 tokenize :: String -> [String]
 tokenize []           = [];
 tokenize('=':'=':xs)  = "==":tokenize xs
@@ -20,6 +24,7 @@ tokenize (x:xs)
   | isAlpha x         = let (word, rest) = span isAlpha (x:xs)
                         in (word:tokenize rest)
   | elem x delimiters = [x]:tokenize xs
+  | isSpace x         = tokenize xs
   | otherwise         = error ("\nInvalid character ----> " ++ [x])
 
 parse :: String -> Ast
@@ -37,6 +42,9 @@ parseExpr :: [String] -> (Ast, [String])
 parseExpr (x:xs)
   | isDigit (head x) = (Number (read x), xs)
   | x == "("         = parseApp (x:xs)
+  | x == "case"      = parseCase xs
+  | x == "set"       = parseSet xs
+  | isAlpha (head x) = parseVar (x:xs)
   | otherwise        = error ("\nInvalid syntax -----> " ++ show(x:xs))
 
 parseApp :: [String] -> (Ast, [String])
@@ -45,3 +53,32 @@ parseApp("(":xs) = ((App (Name operator) [expr_1, expr_2], drop 2 rest_2))
     (expr_1, ",":rest_1) = parseExpr xs
     (expr_2, rest_2)     = parseExpr rest_1
     operator             = rest_2 !! 1
+
+
+parseCase :: [String] -> (Ast, [String])
+parseCase("otherwise":"->":xs)  = let (expr, ".":rest)    = parseExpr xs
+                                  in (Case Default [expr], rest)
+parseCase("(":xs)               = let (bool, rest_1)      = parseBool xs
+                                      (expr, ",":rest_2)  = parseExpr rest_1
+                                      (other, rest_3)     = parseExpr rest_2
+                                  in (Case bool [expr, other], rest_3)
+
+
+
+parseBool :: [String] -> (Ast, [String])
+parseBool xs = let (expr_1, ",":rest_1) = parseExpr xs
+                   (expr_2, rest_2)     = parseExpr rest_1
+                   eq                   = rest_2 !! 1
+               in (Bool (Name eq) expr_1 expr_2, drop 3 rest_2)
+
+parseSet :: [String] -> (Ast, [String])
+parseSet (x:xs)
+  | isReservedWord x = error ("\nInvalid variable name " ++ show(x) ++ ". That is a reserved word!")
+  | otherwise        = let (expr, rest) = parseExpr xs in (Set x expr, rest)
+
+
+parseVar :: [String] -> (Ast, [String])
+parseVar (x:xs) = (Name x, xs)
+
+
+
