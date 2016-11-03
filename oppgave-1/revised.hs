ee653e68a3bb3176d87d56723b1d7393e6b819c3
kaataknut <kaataknut@gmail.com>
2016-11-03 10:31:28 +0100

    Refactored Oblig12016.hs

--- oppgave-1/revised.hs
+++ oppgave-1/revised.hs
@@ -7,13 +7,13 @@ import Data.List
 data Ast = Number Integer | Name String | App Ast [Ast] | Block [Ast] | Case Ast [Ast] | Bool Ast Ast Ast | Default | Set String Ast | Lambda String Ast
   deriving (Eq, Show, Ord)
 
-
--- Helper functions
+-- General Helper functions
 isReservedWord :: String -> Bool
 isReservedWord str = elem str ["set", "lambda", "case", "otherwise"]
 
 delimiters = "+-*/,;()."
 
+-- Tokenizer
 tokenize :: String -> [String]
 tokenize []           = [];
 tokenize('=':'=':xs)  = "==":tokenize xs
@@ -28,6 +28,7 @@ tokenize (x:xs)
   | isSpace x         = tokenize xs
   | otherwise         = error ("\nInvalid character ----> " ++ [x])
 
+-- Parsers
 parse :: String -> Ast
 parse = fst . parseBlock . tokenize
 
@@ -64,8 +65,6 @@ parseCase("(":xs)               = let (bool, rest_1)      = parseBool xs
                                       (other, rest_3)     = parseExpr rest_2
                                   in (Case bool [expr, other], rest_3)
 
-
-
 parseBool :: [String] -> (Ast, [String])
 parseBool xs = let (expr_1, ",":rest_1) = parseExpr xs
                    (expr_2, rest_2)     = parseExpr rest_1
@@ -76,7 +75,6 @@ parseSet :: [String] -> (Ast, [String])
 parseSet (x:xs)
   | isReservedWord x = error ("\nInvalid variable name " ++ show(x) ++ ". That is a reserved word!")
   | otherwise        = let (expr, rest) = parseExpr xs in (Set x expr, rest)
-
 
 parseVar :: [String] -> (Ast, [String])
 parseVar (x:xs) = (Name x, xs)
@@ -111,13 +109,14 @@ addToCtx (Context ctx) name ptr = Context (\str -> if name == str
                                                    then Just ptr
                                                    else ctx str)
 
+-- Helper functions for Eval funtion
 intOps :: [(String, Integer -> Integer -> Integer)]
 intOps = [("+", (+)), ("-", (-)), ("*", (*)), ("/", (div))]
 
 eqOps :: [(String, Integer -> Integer -> Bool)]
 eqOps = [("==", (==)), ("!=", (/=)), (">", (>)), ("<", (<))]
 
--- Eval function that evaluates an Ast
+-- Eval
 eval::Ast -> Context -> Memory -> (Ast, Context, Memory)
 eval (Block [expr]) ctx mem         = eval expr ctx mem
 eval (Block (first:next)) ctx mem   = eval (Block next) ctx2 mem2
@@ -126,6 +125,20 @@ eval (Block (first:next)) ctx mem   = eval (Block next) ctx2 mem2
 
 -- Evaluate Number
 eval (Number a) ctx mem             = (Number a, ctx, mem)
+
+-- Evaluate Set patterns
+eval (Set str expr) ctx mem         = eval ast ctx3 mem3
+  where
+    (ast, ctx2, mem2) = eval expr ctx mem
+    (ptr1, mem3)    = addToMem mem2 ast   -- Add it to the memory
+    ctx3            = addToCtx ctx2 str ptr1 -- Add the variable to the context with the ptr to the address
+
+-- Evaluate a variable
+eval (Name str) ctx mem
+  | isNothing adr                   = error "Variable is not defined!"
+  | otherwise                       = ((fromJust (lookupMem mem (fromJust adr))), ctx, mem)
+  where
+    adr   = lookupCtx ctx str
 
 -- Evaluate App patterns
 eval (App (Name op) [a,b]) ctx mem = let (Number e1, ctx1, mem1) = eval a ctx mem
